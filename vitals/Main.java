6c6d5750e2ca27fa852ad981c7b3ce9f48edc332
Rahul Jain V <38587824+vrahul1998@users.noreply.github.com>
2024-07-03 21:07:28 +0530

    Added test cases and logs, removed reused code

--- vitals/Main.java
+++ vitals/Main.java
@@ -5,39 +5,63 @@ public class Main {
         return temperatureIsOk(temperature) && socIsOk(soc) && chargeRateIsOk(chargeRate);
     }
 
+    static void DisplayOutOfRange(String parameter) {
+        System.out.println(parameter + " is out of range!");
+    }
+
+    private static boolean isWithinRange(float value, float min, float max) {
+        return (min < value) && (max > value);
+    }
+
     static boolean temperatureIsOk(float temperature) {
-        boolean isOk = true;
-        if (temperature < 0) {
-            isOk = false;
+        float min = 0;
+        float max = 45;
+        boolean tempIsOk = isWithinRange(temperature, min, max);
+        if (!tempIsOk) {
+            DisplayOutOfRange("temperature");
         }
-        if (temperature > 45) {
-            isOk = false;
-        }
-        return isOk;
+        return tempIsOk;
     }
 
     static boolean socIsOk(float soc) {
-        boolean isOk = true;
-        if (soc < 20) {
-            isOk = false;
+        float min = 20;
+        float max = 80;
+        boolean socIsOk = isWithinRange(soc, min, max);
+        if (!socIsOk) {
+            DisplayOutOfRange("State of Charge");
         }
-        if (soc > 80) {
-            isOk = false;
-        }
-        return isOk;
+        return socIsOk;
     }
 
     static boolean chargeRateIsOk(float chargeRate) {
-        boolean isOk = true;
-        if (chargeRate > 0.8) {
-            isOk = false;
+        float min = -0.1f;
+        float max = 0.8f;
+        boolean chargeRateOk = isWithinRange(chargeRate, min, max);
+        if (!chargeRateOk) {
+            DisplayOutOfRange("Charge Rate");
         }
-        return isOk;
+        return chargeRateOk;
     }
 
     public static void main(String[] args) {
-        assert(batteryIsOk(25, 70, 0.7f) == true);
-        assert(batteryIsOk(50, 85, 0.0f) == false);
-        System.out.println("Some more tests needed");
+        assert (batteryIsOk(25, 70, 0.7f) == true);
+        assert (batteryIsOk(1, 70, 0.7f) == true);
+        assert (batteryIsOk(44, 70, 0.7f) == true);
+        assert (batteryIsOk(25, 35, 0.7f) == true);
+        assert (batteryIsOk(25, 21, 0.7f) == true);
+        assert (batteryIsOk(25, 79, 0.7f) == true);
+        assert (batteryIsOk(25, 70, 0.0f) == true);
+        assert (batteryIsOk(25, 70, 0.5f) == true);
+        assert (batteryIsOk(25, 70, 0.7f) == true);
+        assert (batteryIsOk(0, 70, 0.7f) == false);
+        assert (batteryIsOk(45, 70, 0.7f) == false);
+        assert (batteryIsOk(25, 20, 0.7f) == false);
+        assert (batteryIsOk(25, 80, 0.7f) == false);
+        assert (batteryIsOk(25, 70, -0.1f) == false);
+        assert (batteryIsOk(25, 70, 0.8f) == false);
+        assert (batteryIsOk(0, 70, 0.8f) == false);
+        assert (batteryIsOk(0, 80, 0.7f) == false);
+        assert (batteryIsOk(1, 20, 0.8f) == false);
+        assert (batteryIsOk(0, 20, 0.8f) == false);
     }
 }
