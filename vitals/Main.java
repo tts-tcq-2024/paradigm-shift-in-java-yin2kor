7a3cd1aa4dded12fcfb7994d1b1925025c5e07f0
Rahul Jain V <38587824+vrahul1998@users.noreply.github.com>
2024-07-24 21:48:46 +0530

    updated test case

--- vitals/Main.java
+++ vitals/Main.java
@@ -13,7 +13,7 @@ public class Main {
         assert (BatteryStatusVerifier.batteryIsOk(25, 70, 0.0f) == true);
         assert (BatteryStatusVerifier.batteryIsOk(25, 70, 0.5f) == true);
         assert (BatteryStatusVerifier.batteryIsOk(25, 70, 0.7f) == true);
-        assert (BatteryStatusVerifier.batteryIsOk(0, 70, 0.7f) == false);
+        assert (BatteryStatusVerifier.batteryIsOk(0, 70, 0.7f) == true);
         assert (BatteryStatusVerifier.batteryIsOk(45, 70, 0.7f) == false);
         assert (BatteryStatusVerifier.batteryIsOk(25, 20, 0.7f) == false);
         assert (BatteryStatusVerifier.batteryIsOk(25, 80, 0.7f) == false);
